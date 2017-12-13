b47b71234c5f21e0845a32a1d2df705d06b7dbea
Joshua Comeau <joshwcomeau@gmail.com>
2017-12-13 09:10:25 -0500

    refactor IntroRouteWaveform to define all variable props in a new method

--- src/components/IntroRouteWaveform/IntroRouteWaveform.js
+++ src/components/IntroRouteWaveform/IntroRouteWaveform.js
@@ -10,6 +10,7 @@ import Waveform from '../Waveform';
 import WaveformPlayer from '../WaveformPlayer';
 import WaveformAxis from '../WaveformAxis';
 import WaveformIntercept from '../WaveformIntercept';
+import FadeTransition from '../FadeTransition';
 
 type Props = {
   currentStep: number,
@@ -17,37 +18,120 @@ type Props = {
   progress: number,
 };
 
+type StepData = {
+  isPlaying: boolean,
+  waveformColor: string,
+  waveformOpacity: number,
+  // TODO: should just use `xAxisOpacity`. When opacity is 0, we can choose
+  // not to render within the component (or just keep it hidden)
+  showXAxis: boolean,
+  showYAxis: boolean,
+  showXAxisLabels: boolean,
+  showYAxisLabels: boolean,
+  xAxisOpacity: number,
+  yAxisOpacity: number,
+};
+
 class IntroRouteWaveform extends Component<Props> {
+  getDataForStep = (step: number): StepData => {
+    const defaults = {
+      isPlaying: false,
+      waveformColor: COLORS.blue[500],
+      waveformOpacity: 1,
+      showXAxis: true,
+      showYAxis: true,
+      showXAxisLabels: false,
+      showYAxisLabels: false,
+      xAxisOpacity: 1,
+      yAxisOpacity: 1,
+    };
+    switch (step) {
+      case 0: {
+        return {
+          ...defaults,
+          showYAxis: false,
+        };
+      }
+
+      case 1: {
+        return {
+          ...defaults,
+          isPlaying: true,
+          showYAxis: false,
+        };
+      }
+
+      case 2: {
+        return defaults;
+      }
+
+      case 3: {
+        return {
+          ...defaults,
+          waveformOpacity: 0.5,
+          showXAxisLabels: true,
+          yAxisOpacity: 0.2,
+        };
+      }
+
+      case 4: {
+        return {
+          ...defaults,
+          waveformOpacity: 0.5,
+          showYAxisLabels: true,
+          xAxisOpacity: 0.2,
+        };
+      }
+
+      default:
+        console.error(
+          'Unrecognized step number!! Returning default values for waveform'
+        );
+        return defaults;
+    }
+  };
+
   renderContents = (width: number) => {
-    const { currentStep } = this.props;
-
-    const isPlaying = [1].includes(currentStep);
-
-    const showXAxis = true;
-    const showXAxisLabels = [3].includes(currentStep);
+    const stepData = this.getDataForStep(this.props.currentStep);
 
     return (
-      <WaveformPlayer isPlaying={isPlaying} numOfCycles={1} speed={0.75}>
+      <WaveformPlayer
+        isPlaying={stepData.isPlaying}
+        numOfCycles={1}
+        speed={0.75}
+      >
         {({ progress, offset, numOfCycles }) => (
           <Aux>
             <Waveform
-              color={COLORS.blue[500]}
+              color={stepData.waveformColor}
               strokeWidth={5}
+              opacity={stepData.waveformOpacity}
               size={width}
               shape="sine"
               offset={offset}
               numOfCycles={numOfCycles}
             />
-            {showXAxis && (
+            <FadeTransition isVisible={stepData.showXAxis}>
               <WaveformAxis
                 x
                 strokeWidth={4}
                 waveformSize={width}
                 numOfCycles={numOfCycles}
                 progress={progress}
-                showLabels={showXAxisLabels}
+                showLabels={stepData.showXAxisLabels}
+                opacity={stepData.xAxisOpacity}
               />
-            )}
+            </FadeTransition>
+            <FadeTransition isVisible={stepData.showYAxis}>
+              <WaveformAxis
+                y
+                strokeWidth={4}
+                waveformSize={width}
+                numOfCycles={numOfCycles}
+                progress={progress}
+                opacity={stepData.yAxisOpacity}
+              />
+            </FadeTransition>
           </Aux>
         )}
       </WaveformPlayer>
