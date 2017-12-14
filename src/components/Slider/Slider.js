a9148e2fa7a67be015643ecc860077b25a564135
Josh Comeau <joshua@khanacademy.org>
2017-12-14 12:07:19 -0500

    Add ReactSlider, create thin wrapper

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,72 @@
+// @flow
+import React, { Component } from 'react';
+import ReactSlider from 'react-slider';
+import { injectGlobal } from 'styled-components';
+
+import { COLORS } from '../../constants';
+
+type Props = {
+  width: number,
+  min?: number,
+  max?: number,
+  step?: number,
+  value?: number,
+  defaultValue?: number,
+  withBars?: boolean,
+  onChange: (val: number) => void,
+};
+
+class Slider extends Component<Props> {
+  static defaultProps = {
+    width: 100,
+  };
+
+  componentDidMount() {
+    // Argh. So, the React Slider library has some sort of bug with default
+    // values. Requires a re-render to set to the right value :/
+    // TODO: Use a different library
+    window.setTimeout(() => this.forceUpdate(), 0);
+  }
+
+  render() {
+    const { width, ...delegatedProps } = this.props;
+
+    return (
+      <div style={{ width }}>
+        <ReactSlider {...delegatedProps} className="slider-component" />
+      </div>
+    );
+  }
+}
+
+// NOTE: React-slider uses specific class names to style
+injectGlobal`
+  .slider-component {
+    height: 20px;
+  }
+
+  .slider-component .bar {
+    top: 10px;
+    height: 4px;
+    transform: translateY(-50%);
+  }
+
+  .slider-component .bar-0 {
+    background: ${COLORS.gray[700]};
+  }
+
+  .slider-component .bar-1 {
+    background: ${COLORS.gray[300]};
+  }
+
+  .slider-component .handle {
+    position: absolute;
+    background: ${COLORS.green[500]};
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.15);
+  }
+`;
+
+export default Slider;
---
+++ src/components/Slider/Slider.stories.js
@@ -0,0 +1,48 @@
+// @flow
+import React, { PureComponent } from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import Slider from './Slider';
+
+type State = {
+  value: number,
+};
+class SliderWrapper extends PureComponent<Any, State> {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (value: number) => {
+    this.setState({ value });
+
+    // Delegate to a supplied function as well
+    this.props.onChange && this.props.onChange(value);
+  };
+
+  render() {
+    return (
+      <Slider
+        {...this.props}
+        onChange={this.handleChange}
+        value={this.state.value}
+      />
+    );
+  }
+}
+
+storiesOf('Slider', module)
+  .add('default', () => <SliderWrapper onChange={action('Slider change')} />)
+  .add('with bars', () => (
+    <SliderWrapper withBars onChange={action('Slider change')} />
+  ))
+  .add('Different range (-1 to 1, 0.05 step)', () => (
+    <SliderWrapper
+      withBars
+      min={-1}
+      max={1}
+      defaultValue={0}
+      step={0.05}
+      onChange={action('Slider change')}
+    />
+  ));
