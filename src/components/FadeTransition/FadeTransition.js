f62402ae8b9ae78fd218fd7704db02f7bad3e691
Josh Comeau <joshua@khanacademy.org>
2017-12-18 10:02:37 -0500

    ARGH  use correct attribute for pointer-events

--- src/components/FadeTransition/FadeTransition.js
+++ src/components/FadeTransition/FadeTransition.js
@@ -1,47 +1,57 @@
 // @flow
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Transition from 'react-transition-group/Transition';
 
 type Props = {
   isVisible: boolean,
   mountOnEnter: boolean,
   unmountOnExit: boolean,
-  duration?: number,
-  typeName?: string,
+  duration: number,
+  typeName: string,
   children: React$Node,
 };
 
-const FadeTransition = ({
-  isVisible,
-  mountOnEnter,
-  unmountOnExit,
-  duration = 500,
-  typeName = 'span',
-  children,
-}: Props) => {
-  return (
-    <Transition
-      in={isVisible}
-      mountOnEnter={mountOnEnter}
-      unmountOnExit={unmountOnExit}
-      timeout={duration}
-    >
-      {transitionState =>
-        React.createElement(
-          typeName,
-          {
-            style: {
-              position: 'static',
-              transition: `opacity ${duration}ms`,
-              opacity: transitionState === 'entered' ? 1 : 0,
-              pointerEvents: transitionState === 'exited' ? 'none' : 'normal',
+class FadeTransition extends PureComponent<Props> {
+  static defaultProps = {
+    duration: 500,
+    typeName: 'span',
+  };
+
+  render() {
+    const {
+      log,
+      isVisible,
+      mountOnEnter,
+      unmountOnExit,
+      duration = 500,
+      typeName = 'span',
+      children,
+    } = this.props;
+
+    return (
+      <Transition
+        in={isVisible}
+        mountOnEnter={mountOnEnter}
+        unmountOnExit={unmountOnExit}
+        timeout={duration}
+      >
+        {transitionState =>
+          React.createElement(
+            typeName,
+            {
+              style: {
+                position: 'static',
+                transition: `opacity ${duration}ms`,
+                opacity: transitionState === 'entered' ? 1 : 0,
+                pointerEvents: transitionState === 'entered' ? 'auto' : 'none',
+              },
             },
-          },
-          children
-        )
-      }
-    </Transition>
-  );
-};
+            children
+          )
+        }
+      </Transition>
+    );
+  }
+}
 
 export default FadeTransition;
