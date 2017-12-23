921ac317a6be039e8262a4e4853dccb36f4b5805
Joshua Comeau <joshwcomeau@gmail.com>
2017-12-23 16:09:54 -0500

    Whoops, include polyfills

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './polyfills';
+
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
