3b2221956da4d0f3cfe564a97395c5826ad2b535
Joshua Comeau <joshwcomeau@gmail.com>
2017-12-25 07:32:54 -0500

    Refactor FrequencyGraph to be a PureComponent, prep for hover state

--- src/components/FrequencyGraph/FrequencyGraph.js
+++ src/components/FrequencyGraph/FrequencyGraph.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 
 import { DEFAULT_WAVEFORM_SHAPE, COLORS } from '../../constants';
@@ -12,12 +12,16 @@ import type { WaveformShape } from '../../types';
 
 type Props = {
   width?: number,
-  shape?: WaveformShape,
-  baseFrequency?: number,
-  baseAmplitude?: number,
-  xMin?: number,
-  xMax?: number,
-  step?: number,
+  shape: WaveformShape,
+  baseFrequency: number,
+  baseAmplitude: number,
+  xMin: number,
+  xMax: number,
+  step: number,
+};
+
+type State = {
+  hovering: ?string,
 };
 
 const ASPECT_RATIO = 0.6;
@@ -25,124 +29,149 @@ const ASPECT_RATIO = 0.6;
 const VIEWBOX_WIDTH = 100;
 const VIEWBOX_HEIGHT = VIEWBOX_WIDTH * ASPECT_RATIO;
 
-const FrequencyGraph = ({
-  shape = DEFAULT_WAVEFORM_SHAPE,
-  baseFrequency = 1,
-  baseAmplitude = 1,
-  xMin = 0,
-  xMax = 20,
-  step = 1,
-}: Props) => {
-  const harmonics = [
-    { frequency: baseFrequency, amplitude: baseAmplitude },
-    ...getHarmonicsForWave({
-      shape,
-      baseFrequency,
-      baseAmplitude,
-      maxNumberToGenerate: xMax,
-    }),
-  ];
+class FrequencyGraph extends PureComponent<Props, State> {
+  static defaultProps = {
+    shape: DEFAULT_WAVEFORM_SHAPE,
+    baseFrequency: 1,
+    baseAmplitude: 1,
+    xMin: 0,
+    xMax: 20,
+    step: 1,
+  };
 
-  // Filter out any harmonics that are too high-frequency to draw.
-  const drawableHarmonics = harmonics.filter(
-    harmonic => harmonic.frequency < xMax
-  );
+  getXAxisValues = () => {
+    const { xMin, xMax, step } = this.props;
 
-  // For our X axis values, we'll add as many points as are specified by our
-  // props.
-  const xAxisValuesStart = xMin + step;
-  const xAxisValuesEnd = xMax * step;
+    const xAxisValuesStart = xMin + step;
+    const xAxisValuesEnd = xMax * step;
 
-  const xAxisValues = range(xAxisValuesStart, xAxisValuesEnd, step).map(
-    index => ({
-      label: index % 2 === 0 ? index + 'Hz' : '',
-      position: VIEWBOX_WIDTH * (index / xMax / step),
-    })
-  );
+    const xAxisValues = range(xAxisValuesStart, xAxisValuesEnd, step).map(
+      index => ({
+        label: index % 2 === 0 ? index + 'Hz' : '',
+        position: VIEWBOX_WIDTH * (index / xMax / step),
+      })
+    );
 
-  const yAxisValues = range(0, 0.9, 0.1).map(index => ({
-    label: index % 0.5 === 0 ? 1 - index : '',
-    position: VIEWBOX_HEIGHT * (index / 1),
-  }));
+    const yAxisValues = range(0, 0.9, 0.1).map(index => ({
+      label: index % 0.5 === 0 ? 1 - index : '',
+      position: VIEWBOX_HEIGHT * (index / 1),
+    }));
 
-  const amplitudeLines = range(0, 10).map(i => (
-    <AmplitudeLine
-      key={i}
-      x1={0}
-      y1={VIEWBOX_HEIGHT * (i / 10)}
-      x2={VIEWBOX_WIDTH}
-      y2={VIEWBOX_HEIGHT * (i / 10)}
-    />
-  ));
+    return { xAxisValues, yAxisValues };
+  };
 
-  return (
-    <FrequencyGraphSvg viewBox={`0 0 ${VIEWBOX_WIDTH} ${VIEWBOX_HEIGHT}`}>
-      {amplitudeLines}
+  renderBars() {
+    const { baseFrequency, baseAmplitude, shape, xMax } = this.props;
+    const harmonics = [
+      { frequency: baseFrequency, amplitude: baseAmplitude },
+      ...getHarmonicsForWave({
+        shape,
+        baseFrequency,
+        baseAmplitude,
+        maxNumberToGenerate: xMax,
+      }),
+    ];
 
-      {drawableHarmonics.map(({ frequency, amplitude }, index) => {
-        // We need to figure out where in our scale this frequency falls.
-        // If we have a frequency of 5hz when the range is 0-10hz, it should
-        // be drawn halfway through the viewBOx width
-        const progress = frequency / xMax;
-        const xCoordinate = VIEWBOX_WIDTH * progress;
+    // Filter out any harmonics that are too high-frequency to draw.
+    const drawableHarmonics = harmonics.filter(
+      harmonic => harmonic.frequency < xMax
+    );
 
-        const yCoordinate =
-          VIEWBOX_HEIGHT - VIEWBOX_HEIGHT * Math.abs(amplitude);
+    return drawableHarmonics.map(({ frequency, amplitude }, index) => {
+      // We need to figure out where in our scale this frequency falls.
+      // If we have a frequency of 5hz when the range is 0-10hz, it should
+      // be drawn halfway through the viewBOx width
+      const progress = frequency / xMax;
+      const xCoordinate = VIEWBOX_WIDTH * progress;
 
-        return (
-          <Aux key={index}>
-            <Bar
-              x1={xCoordinate}
-              y1={VIEWBOX_HEIGHT}
-              x2={xCoordinate}
-              y2={yCoordinate}
-            />
-          </Aux>
-        );
-      })}
+      const yCoordinate = VIEWBOX_HEIGHT - VIEWBOX_HEIGHT * Math.abs(amplitude);
 
-      {/* X Axis */}
-      <Axis
-        x1={0}
-        y1={VIEWBOX_HEIGHT}
-        x2={VIEWBOX_WIDTH + 3}
-        y2={VIEWBOX_HEIGHT}
-      />
-      <AxisLabel x={VIEWBOX_WIDTH} y={VIEWBOX_HEIGHT} dx={-13} dy={10}>
-        Frequency
-      </AxisLabel>
-
-      {/* Y Axis */}
-      <Axis x1={0} y1={-3} x2={0} y2={VIEWBOX_HEIGHT} />
-      <AxisLabel x={0} y={0} dx={-10} dy={0} transform="rotate(270,23,27)">
-        Amplitude
-      </AxisLabel>
-
-      {xAxisValues.map(({ label, position }, index) => (
+      return (
         <Aux key={index}>
-          <AxisNub
-            x1={position}
+          <Bar
+            x1={xCoordinate}
             y1={VIEWBOX_HEIGHT}
-            x2={position}
-            y2={VIEWBOX_HEIGHT + (label ? 2 : 1)}
+            x2={xCoordinate}
+            y2={yCoordinate}
           />
-          <AxisNubLabel x={position} y={VIEWBOX_HEIGHT + 5} dx={-2}>
-            {label}
-          </AxisNubLabel>
         </Aux>
-      ))}
+      );
+    });
+  }
 
-      {yAxisValues.map(({ label, position }, index) => (
-        <Aux key={index}>
-          <AxisNub x1={label ? -2 : -1} y1={position} x2={0} y2={position} />
-          <AxisNubLabel x={-5} y={position} dx={-0.5} dy={0.85}>
-            {label}
-          </AxisNubLabel>
-        </Aux>
-      ))}
-    </FrequencyGraphSvg>
-  );
-};
+  render() {
+    // const {
+    //   shape,
+    //   baseFrequency,
+    //   baseAmplitude,
+    //   xMin,
+    //   xMax,
+    //   step,
+    // } = this.props;
+
+    // For our X axis values, we'll add as many points as are specified by our
+    // props.
+    const { xAxisValues, yAxisValues } = this.getXAxisValues();
+
+    const backgroundLines = range(0, 10).map(i => (
+      <BackgroundLine
+        key={i}
+        x1={0}
+        y1={VIEWBOX_HEIGHT * (i / 10)}
+        x2={VIEWBOX_WIDTH}
+        y2={VIEWBOX_HEIGHT * (i / 10)}
+      />
+    ));
+
+    return (
+      <FrequencyGraphSvg viewBox={`0 0 ${VIEWBOX_WIDTH} ${VIEWBOX_HEIGHT}`}>
+        {backgroundLines}
+
+        {this.renderBars()}
+
+        {/* X Axis */}
+        <Axis
+          x1={0}
+          y1={VIEWBOX_HEIGHT}
+          x2={VIEWBOX_WIDTH + 3}
+          y2={VIEWBOX_HEIGHT}
+        />
+        <AxisLabel x={VIEWBOX_WIDTH} y={VIEWBOX_HEIGHT} dx={-13} dy={10}>
+          Frequency
+        </AxisLabel>
+
+        {/* Y Axis */}
+        <Axis x1={0} y1={-3} x2={0} y2={VIEWBOX_HEIGHT} />
+        <AxisLabel x={0} y={0} dx={-10} dy={0} transform="rotate(270,23,27)">
+          Amplitude
+        </AxisLabel>
+
+        {xAxisValues.map(({ label, position }, index) => (
+          <Aux key={index}>
+            <AxisNub
+              x1={position}
+              y1={VIEWBOX_HEIGHT}
+              x2={position}
+              y2={VIEWBOX_HEIGHT + (label ? 2 : 1)}
+            />
+            <AxisNubLabel x={position} y={VIEWBOX_HEIGHT + 5} dx={-2}>
+              {label}
+            </AxisNubLabel>
+          </Aux>
+        ))}
+
+        {yAxisValues.map(({ label, position }, index) => (
+          <Aux key={index}>
+            <AxisNub x1={label ? -2 : -1} y1={position} x2={0} y2={position} />
+            <AxisNubLabel x={-5} y={position} dx={-0.5} dy={0.85}>
+              {label}
+            </AxisNubLabel>
+          </Aux>
+        ))}
+      </FrequencyGraphSvg>
+    );
+  }
+}
 
 const FrequencyGraphSvg = styled.svg`
   overflow: visible;
@@ -167,7 +196,7 @@ const AxisNub = styled.line`
   stroke-width: 0.25;
 `;
 
-const AmplitudeLine = styled.line`
+const BackgroundLine = styled.line`
   stroke: ${COLORS.gray[300]};
   stroke-width: 0.1;
 `;
