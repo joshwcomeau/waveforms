f4bf4ec30666b5eee14e0275d29c5aa294925b40
Joshua Comeau <joshwcomeau@gmail.com>
2017-12-31 08:29:22 -0500

    Fix Oscillator stories

--- src/components/Oscillator/Oscillator.stories.js
+++ src/components/Oscillator/Oscillator.stories.js
@@ -6,16 +6,23 @@ import { action } from '@storybook/addon-actions';
 import Oscillator from './Oscillator';
 
 storiesOf('Oscillator', module)
-  .add('Silent', () => <Oscillator frequency={440} amplitude={1} />)
+  .add('Silent (muted with masterVolume)', () => (
+    <Oscillator masterVolume={0} frequency={440} amplitude={1} />
+  ))
   .add('440Hz, default shape', () => (
-    <Oscillator isAudible frequency={440} amplitude={1} />
+    <Oscillator masterVolume={0.5} frequency={440} amplitude={1} />
   ))
   .add('220Hz, default shape', () => (
-    <Oscillator isAudible frequency={220} amplitude={1} />
+    <Oscillator masterVolume={0.5} frequency={220} amplitude={1} />
   ))
   .add('220Hz, sawtooth', () => (
-    <Oscillator isAudible shape="sawtooth" frequency={220} amplitude={1} />
+    <Oscillator
+      masterVolume={0.5}
+      shape="sawtooth"
+      frequency={220}
+      amplitude={1}
+    />
   ))
-  .add('220Hz, quiet', () => (
-    <Oscillator isAudible frequency={220} amplitude={0.5} />
+  .add('220Hz, LOUD', () => (
+    <Oscillator masterVolume={1} frequency={220} amplitude={1} />
   ));
