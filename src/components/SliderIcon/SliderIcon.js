a6357b60f3f9dbdc5a304ca14f31ffc5efabe006
Joshua Comeau <joshwcomeau@gmail.com>
2018-01-02 08:25:36 -0500

    Create SliderIcon component

---
+++ src/components/SliderIcon/SliderIcon.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Aux from '../Aux';
+
+import { COLORS } from '../../constants';
+
+const SliderIcon = ({ fieldName }) => {
+  return (
+    <Wrapper>
+      <MiniSlider>
+        <Bar1 />
+        <Bar2 />
+        <Handle />
+      </MiniSlider>{' '}
+      <strong>{fieldName} slider</strong>
+    </Wrapper>
+  );
+};
+
+const HANDLE_SIZE = 13;
+
+const Wrapper = styled.span`
+  white-space: nowrap;
+`;
+
+const MiniSlider = styled.span`
+  position: relative;
+  display: inline-block;
+  width: 30px;
+  height: ${HANDLE_SIZE}px;
+  vertical-align: middle;
+  margin-left: 4px;
+`;
+
+const Bar = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin-top: auto;
+  margin-bottom: auto;
+  height: 2px;
+  width: 50%;
+`;
+
+const Bar1 = styled(Bar)`
+  left: 0;
+  background-color: ${COLORS.gray[700]};
+`;
+const Bar2 = styled(Bar)`
+  right: 0;
+  background-color: ${COLORS.gray[400]};
+`;
+
+const Handle = styled.div`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  height: ${HANDLE_SIZE}px;
+  width: ${HANDLE_SIZE}px;
+  border-radius: 50%;
+  background: ${COLORS.primary[500]};
+  transform: translateX(-50%);
+`;
+
+export default SliderIcon;
